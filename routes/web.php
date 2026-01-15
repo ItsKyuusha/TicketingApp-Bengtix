89b28d00c88fc1707b3ae005d31c1fd9f8262236
Kyuusha <daaddimaki04@gmail.com>
2026-01-16 01:33:07 +0700

    build feature manage events admin

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\Admin\DashboardController;
 use App\Http\Controllers\Admin\CategoryController;
+use App\Http\Controllers\Admin\EventController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -26,6 +27,9 @@ Route::middleware('auth')->group(function () {
 
         // Category Management
         Route::resource('categories', CategoryController::class);
+
+        // Event Management
+        Route::resource('events', EventController::class);
     });
 });
 
